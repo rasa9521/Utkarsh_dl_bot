9ccd4ff1f5771e41be4caf91d39b10d58252ac62
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-07-13 17:04:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Utkarsh_dl_bot
+
+# Deploy To Heroku
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/pkvgithub/Ut_dl_bot)
---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "0")
+    API_ID = int(os.environ["API_ID", ]
+    API_HASH = os.environ["API_HASH", "0"]
+    AUTH_USERS = "7163346348 """
